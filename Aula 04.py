db188ded5608f9611751eccf3940426a49f96a42
EvelynSantos6 <evelyn.santos20032@gmail.com>
2025-08-12 21:04:33 -0300

    Aula 04 - Construindo um Dashboard com Streamlit

--- Aula 04.py
+++ Aula 04.py
@@ -42,7 +42,7 @@ df_filtrado = df[
 ]
 
 # --- Conteúdo Principal ---
-st.title("🎲 Dashboard de Análise de Salários na Área de Dados")
+st.title("🖥️ Dashboard de Análise de Salários na Área de Dados")
 st.markdown("Explore os dados salariais na área de dados nos últimos anos. Utilize os filtros à esquerda para refinar sua análise.")
 
 # --- Métricas Principais (KPIs) ---
